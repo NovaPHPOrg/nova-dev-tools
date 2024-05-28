c6e7ed7d5a98bc51fff96ff40432627f38c9fc11
Ankio <ankio@ankio.net>
2024-05-28 16:27:10 +0800

    :bug: 修复创建路径错误的问题

--- src/commands/init/InitCommand.php
+++ src/commands/init/InitCommand.php
@@ -8,6 +8,7 @@ use const nova\SUPPORTED_PHP_VERSION;
 class InitCommand extends BaseCommand
 {
     private NovaProject $nova;
+    private string $projectDir;
     public function __construct($workingDir, $options)
     {
         parent::__construct($workingDir, $options);
@@ -23,15 +24,15 @@ class InitCommand extends BaseCommand
         $this->nova->author = $this->prompt("请输入作者: ", $this->nova->author);
         $this->nova->license = $this->prompt("请输入许可证: ",$this->nova->license);
         // 创建项目目录
-        $projectDir = $this->workingDir . DIRECTORY_SEPARATOR . $this->nova->name;
-        $this->echoInfo("创建项目目录 {$projectDir}...");
-        if (!file_exists($projectDir)) {
-            mkdir($projectDir);
+        $this->projectDir = $this->workingDir . DIRECTORY_SEPARATOR . $this->nova->name;
+        $this->echoInfo("创建项目目录 {$this->projectDir}...");
+        if (!file_exists($this->projectDir)) {
+            mkdir($this->projectDir);
             // 初始化git
-            shell_exec("cd {$projectDir} && git init");
+            shell_exec("cd {$this->projectDir} && git init");
             // 创建nova.json
             $json = json_encode($this->nova, JSON_PRETTY_PRINT);
-            file_put_contents($projectDir . DIRECTORY_SEPARATOR . "nova.json", $json);
+            file_put_contents($this->projectDir . DIRECTORY_SEPARATOR . "nova.json", $json);
 
             $dirs = [
                 "src",
@@ -42,9 +43,10 @@ class InitCommand extends BaseCommand
                 "src/nova/plugins",//nova插件目录
             ];
             foreach ($dirs as $dir) {
-                mkdir($projectDir . DIRECTORY_SEPARATOR . $dir, 0777, true);
+                $dir = $this->getDir($dir);
+                mkdir($this->projectDir . DIRECTORY_SEPARATOR . $dir, 0777, true);
                 // 创建.gitkeep
-                file_put_contents($projectDir . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . ".gitkeep", "");
+                file_put_contents($this->projectDir . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . ".gitkeep", "");
             }
 
             $this->initFramework();
@@ -53,6 +55,9 @@ class InitCommand extends BaseCommand
             $this->echoError("项目目录已存在。");
             exit();
         }
+    }
+    private function getDir($dir){
+        return str_replace("/", DIRECTORY_SEPARATOR, $dir);
     }
     private function initReadme()
     {
@@ -64,7 +69,7 @@ class InitCommand extends BaseCommand
 # License
 {$this->nova->license}
 EOF;
-        file_put_contents($this->workingDir . DIRECTORY_SEPARATOR . "README.md", $readme);
+        file_put_contents($this->projectDir . DIRECTORY_SEPARATOR . "README.md", $readme);
     }
     private function initComposer(){
         $composer = json_encode([
@@ -84,7 +89,7 @@ EOF;
                 ]
             ]
         ], JSON_PRETTY_PRINT);
-        file_put_contents($this->workingDir . DIRECTORY_SEPARATOR . "composer.json", $composer);
+        file_put_contents($this->projectDir . DIRECTORY_SEPARATOR . "composer.json", $composer);
     }
 
     private function initIgnore()
@@ -93,8 +98,8 @@ EOF;
 /vendor
 composer.lock
 EOF;
-        file_put_contents($this->workingDir . DIRECTORY_SEPARATOR . ".gitignore", $ignore);
-        shell_exec("cd {$this->workingDir} && git add . && git commit -m ':tada:  init {$this->nova->name}'");
+        file_put_contents($this->projectDir . DIRECTORY_SEPARATOR . ".gitignore", $ignore);
+        shell_exec("cd {$this->projectDir} && git add . && git commit -m ':tada:  init {$this->nova->name}'");
     }
 
     private function initPublic(){
@@ -103,7 +108,7 @@ EOF;
 require __DIR__ . '/../vendor/autoload.php';
 //TODO 入口文件，App启动
 EOF;
-        file_put_contents($this->workingDir . DIRECTORY_SEPARATOR . "src/public/index.php", $index);
+        file_put_contents($this->projectDir . DIRECTORY_SEPARATOR . $this->getDir("src/public/index.php"), $index);
 
     }
     private function initFramework()
