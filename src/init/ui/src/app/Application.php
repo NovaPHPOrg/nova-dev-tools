10e1ddf7dc4ed0a96d8e69106675dbcf179921b8
ankio <ankio@ankio.net>
2025-03-03 21:22:26 +0800

    :recycle: (app): 重构 Application 类并继承 App 基类
    将 Application 类重构为继承 nova\framework\App 基类，移除了冗余的方法并简化了代码结构。添加了 strict_types 声明和版权注释，提升了代码的可维护性和规范性。

--- src/init/ui/src/app/Application.php
+++ src/init/ui/src/app/Application.php
@@ -1,43 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * Copyright (c) 2025. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 namespace app;
 
+use nova\framework\App;
 use nova\framework\event\EventManager;
 use nova\framework\exception\AppExitException;
-use nova\framework\iApplication;
-use nova\framework\request\Response;
-use nova\framework\request\Route;
-use nova\framework\request\RouteObject;
-use nova\plugin\minify\NovaMinify;
-use nova\plugin\task\Task;
+use nova\framework\http\Response;
+
 use function nova\framework\route;
+
+use nova\framework\route\Route;
+
 use const ROOT_PATH;
 
-class Application implements iApplication
+class Application extends App
 {
-
-
-    function onAppEnd()
-    {
-
-    }
-
-    function onRouteNotFound(?RouteObject $route, string $uri): ?Response
-    {
-        return null;
-    }
-
-    function onApplicationError(?RouteObject $route, string $uri): ?Response
-    {
-        return null;
-    }
-
-    function onRoute(RouteObject $route)
-    {
-
-    }
-
-    function routeStatic(): void
+    public function routeStatic(): void
     {
         EventManager::addListener("response.static.after", function ($event, $file) {
             $name = str_replace(ROOT_PATH . '/app', '', $file);
@@ -50,26 +38,18 @@ window.novaFiles['$name'] = true;
 EOF;
 
         });
-        EventManager::addListener("route.before",function ($event, &$uri){
-            if (str_starts_with($uri,"/static/")){
-                $file = substr($uri,8);
-                $file = str_replace("..","",$file);
-                throw new AppExitException(Response::asStatic(ROOT_PATH.'/app/static/'.$file),"Send static file");
+        EventManager::addListener("route.before", function ($event, &$uri) {
+            if (str_starts_with($uri, "/static/")) {
+                $file = substr($uri, 8);
+                $file = str_replace("..", "", $file);
+                throw new AppExitException(Response::asStatic(ROOT_PATH.'/app/static/'.$file), "Send static file");
             }
         });
     }
-    function onFrameworkStart(): void
+
+    public function onFrameworkStart(): void
     {
         $this->routeStatic();
-    }
-
-    function onFrameworkEnd()
-    {
-
-    }
-
-    function onAppStart()
-    {
 
     }
 }
