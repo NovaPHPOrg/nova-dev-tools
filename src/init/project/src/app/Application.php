fa9bad6d8b5f8f6687412f7c191c931bf0a66fd7
Ankio <ankio@ankio.net>
2025-07-15 09:59:17 +0800

    :sparkles: (app): 初始化应用框架并更新配置
    - 新增 Application.php 作为应用主入口类
    - 设置系统名称为 "Nova App"
    - 更新 config.php 中版本号为 1.0.0
    - 移除不再使用的缓存驱动配置

---
+++ src/init/project/src/app/Application.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) 2025. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+namespace app;
+
+use nova\framework\App;
+
+use const ROOT_PATH;
+
+class Application extends App
+{
+
+    public function onFrameworkStart(): void
+    {
+
+    }
+
+    public const string SYSTEM_NAME =  "Nova App";
+
+}
