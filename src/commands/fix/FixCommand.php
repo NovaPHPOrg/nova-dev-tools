c8b970592e0b109304e5d6e5bb58a379ab189084
Ankio <ankio@ankio.net>
2025-07-15 09:56:10 +0800

    :art: (fix): 使用类方法替换直接调用 shell_exec
    在 FixCommand 中，将直接调用的 shell_exec 替换为类的 exec 方法，提高代码封装性和可测试性

--- src/commands/fix/FixCommand.php
+++ src/commands/fix/FixCommand.php
@@ -10,9 +10,9 @@ class FixCommand extends BaseCommand
     public function init(): void
     {
         $this->echoInfo("fix code ....");
-        shell_exec("php php-cs-fixer.phar fix src --allow-risky=yes");
-        shell_exec('git add -- "src/**/*.php"');
-        shell_exec('git commit -m ":art: Code formatting"');
+        $this->exec("php php-cs-fixer.phar fix src --allow-risky=yes");
+        $this->exec('git add -- "src/**/*.php"');
+        $this->exec('git commit -m ":art: Code formatting"');
         $this->echoSuccess("fix code success");
     }
 }
